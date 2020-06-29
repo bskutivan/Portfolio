9e8dfb4ec81797911b7c2df5b9e55b03608b58e6
BrandonKutivan <bskutivan@gmail.com>
2020-06-28 17:01:22 -0700

    updated bootstrap and added new project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 project1El = document.getElementById("project1");
+project5El = document.getElementById("project5");
 
-function projectClickHandler() {
+function project1ClickHandler() {
     event.preventDefault();
 
     console.log(event.target);
     window.location.href="https://github.com/bskutivan/National-Park-Planner"
 }
 
+function project1ClickHandler() {
+    event.preventDefault();
+
+    console.log(event.target);
+    window.location.href="https://github.com/bskutivan/character_builder"
+}
 
 
-project1El.addEventListener("click", projectClickHandler);
+project1El.addEventListener("click", project1ClickHandler);
+project5El.addEventListener("click", project5ClickHandler)
